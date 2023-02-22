f4607da7e8c4206a325ff339e840b5a9f71eed9b
Noormees <garry.lobanoff@gmail.com>
2023-02-22 20:33:14 +0200

    Add Week7

---
+++ Arguments_Parameters/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+Console.WriteLine("Enter a word:");
+string UserInput = Console.ReadLine();
+
+PrintAnyWord(UserInput);                        // UserInput - argument
+
+static void PrintAnyWord(string anyString)      //anyString - parameter
+{
+    anyString = anyString.ToUpper();
+
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(anyString);
+    }
+}
+
---
+++ Week7_Functions/Program.cs
@@ -0,0 +1,34 @@
+﻿
+Console.WriteLine("Are you coming or leaving? (in/out)");
+string userCoise = Console.ReadLine();
+
+if (userCoise == "in")
+{
+    PrintHello();
+}
+else if (userCoise == "out")
+{
+    PrintGoodbye();
+}
+else 
+{
+    Console.WriteLine("You are wrong way!");
+}
+
+
+
+
+//PrintHello();
+//PrintGoodbye();
+
+
+static void PrintHello()     // funktsioon e.meetod
+{ 
+Console.WriteLine("Hello, world!");
+}
+
+
+static void PrintGoodbye()
+{
+    Console.WriteLine("See you later!");
+}
